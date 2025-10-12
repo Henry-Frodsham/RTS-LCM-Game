da941ee554f57658bd5329d8c99a7e6f43269716
Henry Frodsham <ZMAC367@live.rhul.ac.uk>
2025-10-12 15:52:21 +0100

    Event bus tests and pre build script optimisation

--- product/tests/EventBusTests.cpp
+++ product/tests/EventBusTests.cpp
@@ -1,14 +1,41 @@
 #include <doctest/doctest.h>
+#include <string>
 #include "EventBus.h"
 
+struct TestEvent {
+	std::string data = "test";
+};
+
+void TestHandler(TestEvent) {
+
+}
+
 TEST_CASE("EventBus - constructor") {
-	EventBus obj();
+	CHECK_NOTHROW(EventBus obj());
 }
 
-TEST_CASE("EventBus - register") {
+TEST_CASE("EventBus - subscribe and publish") {
+    EventBus Obj{};
+    bool HandlerCalled = false;
 
+    Obj.Subscribe<TestEvent>([&HandlerCalled](const TestEvent& event) {
+        HandlerCalled = true;
+        });
+
+    Obj.Publish(TestEvent());
+
+    CHECK(HandlerCalled);
 }
 
-TEST_CASE("EventBus - subscribe") {
+TEST_CASE("EventBus - singular handler call from publish") {
+    EventBus Obj{};
+    int NumCalls = 0;
 
+    Obj.Subscribe<TestEvent>([&NumCalls](const TestEvent& event) {
+        NumCalls += 1;
+        });
+
+    Obj.Publish(TestEvent());
+
+    CHECK(NumCalls == 1);
 }
