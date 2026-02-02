604be5a689da7ef37e33a38e9e9fd3336b472300
Henry <ZMAC367@live.rhul.ac.uk>
2026-02-02 13:06:32 +0000

    implemented cursor visibility, cursor is only visibile for controllers, fixed pointer declaration in interaction tests

--- product/MatrixableInteractionEvent.h
+++ product/MatrixableInteractionEvent.h
@@ -4,7 +4,7 @@
 //definitions for all matrixable interactions, these are by nature very simple (thus can be evaluated by a matrix)
 struct AttackEvent {
   int AttackPower;
-  int &DefendingEntityHP;
-  AttackEvent(int Power, int &DefendingHP)
+  int* DefendingEntityHP;
+  AttackEvent(int Power, int* DefendingHP)
       : AttackPower(Power), DefendingEntityHP(DefendingHP){}
 };
